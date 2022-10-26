abc8b60473b1f31bb295fd5e7fe24ee46d6ab40c
phuoc <phuoc.khac@sodaq.com>
2022-10-26 15:08:34 +0200

    Added comments

--- DUTsim.py
+++ DUTsim.py
@@ -5,11 +5,13 @@ import time
 from datetime import timedelta
 import serial as ser
 
+
+#Set up serial port (COM# on Windows, /dev/ttyUSB# on Linux)
 ser = serial.Serial()
 port = input("Enter Serial port: ")
 ser = serial.Serial(
     port = port,
-    baudrate = 115200,
+    baudrate = 115200, #Standard 115200 8N1 UART setup
     parity=serial.PARITY_NONE,
     stopbits=serial.STOPBITS_ONE,
     bytesize=serial.EIGHTBITS,
@@ -17,11 +19,11 @@ ser = serial.Serial(
 )
 ser.setDTR(False)
 ser.setRTS(False)
-print("Start reading in 2 seconds...")
+print("Start sending in 2 seconds...")
 time.sleep(2)
 ser.isOpen()
 
-# Create datetime string
+# Create datetime string (just to test with different timestamps)
 time_str = "[23:02:11:430]"
 def time_extract(time_str):
     hour = int(time_str[1:3])
@@ -31,16 +33,18 @@ def time_extract(time_str):
     current_time = timedelta(hours= hour, minutes= minute, seconds= second, microseconds= millisecond*1000)
     return current_time
 
-#Quick calculating two timestamps 
+#Quickly calculate two timestamps (Test)
 timeA = timedelta(hours=0, minutes=6, seconds=59, milliseconds=478)
 timeB = timedelta(hours=0, minutes=7, seconds=1, milliseconds= 468)
-
 timeC = timeB - timeA
 #print(timeC.microseconds)
 
+#Start Replaying !!
 with open('LOG00258.txt') as f:
     previous_time = timedelta()
     current_time =  timedelta()
+
+
     for line in f.readlines():
         if (line[0] == '[' and line[13] == ']'):
             
@@ -50,19 +54,20 @@ with open('LOG00258.txt') as f:
                 ser.write(line.encode())
                 current_time =  time_extract(line)
                 continue
-            previous_time = current_time
+            previous_time = current_time  
+
             #print(previous_time)
             current_time =  time_extract(line)
             #print(current_time)
 
-            #If the device traveled back time :)
+            #If the device traveled to the future :)
             if(current_time < previous_time):
                 print(line)
                 ser.write(line.encode())
                 previous_time = time_extract(line)
                 continue
-            delay_time = current_time - previous_time
-            duration = delay_time.seconds + (delay_time.microseconds/1000000)
+            delay_time = current_time - previous_time   #Calculate the delay duration (In timestamp datatype)
+            duration = delay_time.seconds + (delay_time.microseconds/1000000)   #So it will delay accurately in milliseconds
             #print(duration)
             time.sleep(duration)
             print(line)
@@ -71,5 +76,5 @@ with open('LOG00258.txt') as f:
             print(line)
             ser.write(line.encode())
 
-ser.close()
+ser.close() #Nicely close the serial port
 
